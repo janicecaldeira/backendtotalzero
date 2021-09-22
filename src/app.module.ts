7ec313309d23912876c9a281289f999a6bbd9600
janicecaldeira <janicecaldeira@gmail.com>
2021-09-21 22:30:22 -0300

    Atualizando CRUD e ROTAS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:8LsrhQ0c2YcgaAep@cluster0.nlzlu.mongodb.net/test',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
